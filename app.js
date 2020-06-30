73b5597ede86a6702e0651d0e7bfbaad0643576b
Bilal Ahmad <pro123908@gmail.com>
2020-07-01 03:53:35 +0500

    Templating stuff done

--- app.js
+++ app.js
@@ -1,9 +1,33 @@
 const express = require("express");
+const path = require("path");
 const app = express();
+const morgan = require("morgan");
+const expressHandleBars = require("express-handlebars");
+const connectDB = require("./config/db");
 
 const dotenv = require("dotenv");
 
+//Load config
 dotenv.config({ path: "./config/config.env" });
+
+// Database connection
+connectDB();
+
+// static assets
+app.use(express.static(path.join(__dirname, "public")));
+
+// Logging
+process.env.NODE_ENV === "development" ? app.use(morgan("dev")) : null;
+
+// Template
+app.engine(
+  ".hbs",
+  expressHandleBars({ defaultLayout: "main", extname: ".hbs" })
+);
+app.set("view engine", ".hbs");
+
+//Routes
+app.use("/", require("./routes/index"));
 
 const PORT = process.env.PORT || 3001;
 
